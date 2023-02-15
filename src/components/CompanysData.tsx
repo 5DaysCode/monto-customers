1e5741824e790a83ce4f90b11fc7e5867d4bb928
5DaysCode <5DaysCode@gmail.com>
2023-02-15 11:26:56 +0100

    getting fetch

--- src/components/CompanysData.tsx
+++ src/components/CompanysData.tsx
@@ -2,6 +2,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const CompanysData = () => {
+  const [customerCompanies, setCustomerCompanies] = useState();
+
+  const api_url =
+    "https://my-json-server.typicode.com/capcito/frontend-ws/companies";
+
+  useEffect(() => {
+    axios.get(api_url).then((response) => {
+      console.log("Data => ", response.data);
+    });
+  }, []);
+
   return <div>Company Data</div>;
 };
 
